3469fc40d144d205a448c4aa8329c1e05591dc59
Some User <someuser94@mail.ru>
2018-03-18 22:09:55 +0100

    Content styles ready

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -9,11 +9,11 @@ main
         span.g(title="Guest win") 2
     p.row(v-for="(match, index) in matches")
         span.teams {{match.home}} - {{match.guest}}
-        span: input.h(type="button", :value="best(index, '1').coefficient", :title="best(index, '1').name")
-        span: input.hd(type="button", :value="best(index, '10').coefficient", :title="best(index, '1').name")
-        span: input.d(type="button", :value="best(index, '0').coefficient", :title="best(index, '1').name")
-        span: input.dg(type="button", :value="best(index, '02').coefficient", :title="best(index, '1').name")
-        span: input.g(type="button", :value="best(index, '2').coefficient", :title="best(index, '1').name")
+        span.h(:title="best(index, '1').name") {{best(index, '1').coefficient}}
+        span.hd(:title="best(index, '1').name") {{best(index, '10').coefficient}}
+        span.d(:title="best(index, '1').name") {{best(index, '0').coefficient}}
+        span.dg(:title="best(index, '1').name") {{best(index, '02').coefficient}}
+        span.g(:title="best(index, '1').name") {{best(index, '2').coefficient}}
 </template>
 
 <script>
@@ -116,35 +116,36 @@ export default {
 <style lang="less" scoped>
 main {
     margin: 10px 0;
+    padding: 1px 0;
     background: white;
     
     .row {
         display: flex;
         flex-flow: row nowrap;
-        margin: 10px 0;
+        margin: 5px 0;
         
         &.head span {
             cursor: pointer;
         }
         
         span {
-            flex: 1 0 13%;
+            flex: 1 0 calc((65% - 28px) / 5);
             text-align: center;
-            padding: 5px 0;
+            padding: 10px 0;
+            margin: 0 2px;
+            cursor: pointer;
             
-    
             &.teams {
                 flex-basis: 35%;
                 background: lightblue;
-                display: flex;
-                justify-content: center;
-                align-items: center;
+                margin-left: 4px;
             }
+            &.h {background: #00ff00}
+            &.hd {background: #ccff33}
+            &.d {background: #ffff00}
+            &.dg {background: #66ccff}
+            &.g {background: #3399ff; margin-right: 4px;}
             
-            input {
-                cursor: pointer;
-                padding: 5px 15px;
-            }
         }
         
     }
