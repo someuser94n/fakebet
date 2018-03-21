53ab62af40cc24d154e1d9343ff1d366e032c8d3
Some User <someuser94@mail.ru>
2018-03-21 14:46:06 +0100

    Authorization component ready

--- src/components/Authorization.vue
+++ src/components/Authorization.vue
@@ -1,6 +1,23 @@
 <template lang="pug">
-div
+div#auth
     h2 Authorization
+    app-validating-input(
+    v-for="field in fields",
+    :key="field.title",
+    :type="'row'",
+    :title="field.title",
+    :pattern="field.pattern",
+    :value="field.value",
+    :failMessage="field.failMessage",
+    @new-value="field.value=$event"
+    )
+    input#login(
+    type="button",
+    value="Login",
+    @click="login",
+    :disabled="!dataValid || !dataFilled",
+    :title="buttonTitle"
+    )
 </template>
 
 <script>
@@ -10,10 +27,60 @@ export default {
     components: {
         AppValidatingInput
     },
-    
+    data() {
+        return {
+            fields: [
+                {
+                    title: "login",
+                    value: "",
+                    pattern: /^[a-zA-Z _]+$/,
+                    failMessage: "Only latin characters, space or underscore"
+                },
+                {
+                    title: "password",
+                    value: "",
+                    pattern: /^[a-zA-Z0-9]+$/,
+                    failMessage: "Only latin characters and numbers"
+                }
+            ]
+        }
+    },
+    computed: {
+        dataValid() {
+            return this.fields.every(field => field.pattern.test(field.value));
+        },
+        dataFilled() {
+            return this.fields.every(field => field.value !== "");
+        },
+        buttonTitle() {
+            if(!this.dataFilled) return "Fill in the data";
+            if(!this.dataValid) return "Move mouse over the sign ✖";
+            return false;
+        }
+    },
+    methods: {
+        login() {
+            console.log("send");
+        }
+    }
 }
 </script>
 
 <style lang="less">
-
+#auth {
+    background: white;
+    padding: 5px 0;
+    
+    h2 {
+        margin: 0;
+        text-align: center;
+    }
+    
+    #login {
+        display: block;
+        margin: 5px auto;
+        padding: 3px 30px;
+        font-size: 15px;
+    }
+}
 </style>
--- src/components/ValidatingInput.vue
+++ src/components/ValidatingInput.vue
@@ -1,10 +1,16 @@
 <template lang="pug">
-div
-    label {{title}} {{icon}}
-        input(
-        @input="onInput($event.target.value)"
-        )
-
+label
+    span.icon(
+    v-if="activated",
+    v-html="icon.context",
+    :class="icon.className",
+    :title="icon.title"
+    )
+    span Enter {{title}}
+    input(
+    :value="value",
+    @input="onInput($event.target.value)"
+    )
 </template>
 
 <script>
@@ -13,19 +19,62 @@ export default {
     props: {
         title: String,
         value: String,
-        pattern: [Object, Function]
+        pattern: RegExp,
+        failMessage: String
+    },
+    data() {
+        return {
+            activated: false,
+            icons: {
+                valid: {
+                    className: "valid",
+                    context: "&#10004;",
+                    title: "OK"
+                },
+                failed: {
+                    className: "failed",
+                    context: "&#10006;",
+                    title: this.failMessage
+                }
+            }
+        }
     },
     computed: {
         valid() {
-            return typeof this.pattern === "function" ? this.pattern(this.value): this.pattern.test(this.value);
+            return this.pattern.test(this.value);
         },
         icon() {
-            return this.valid ? "&#10004;" : "&#10006;";
+            return this.valid ? this.icons["valid"] : this.icons["failed"];
+        }
+    },
+    methods: {
+        onInput(value) {
+            this.activated = true;
+            this.$emit("new-value", value);
         }
     }
 }
 </script>
 
-<style lang="less">
-
+<style lang="less" scoped>
+label {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 18px;
+    margin: 10px 0;
+    
+    & > * {
+        flex: 0 0 150px
+    }
+    
+    .icon {
+        text-align: right;
+        margin-right: 10px;
+        flex-basis: 20px;
+        
+        &.valid {color: green;}
+        &.failed {color: red;}
+    }
+}
 </style>
