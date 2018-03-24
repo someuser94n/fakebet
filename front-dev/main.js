9a37160100ff39b8c50e632ead3bc3cb4c3a9f29
Some User <someuser94@mail.ru>
2018-03-24 16:51:44 +0100

    Added axios

--- front-dev/main.js
+++ front-dev/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+Vue.use(VueAxios, axios);
+
 import {router} from "./router";
 import {i18n} from "./i18n/";
 
