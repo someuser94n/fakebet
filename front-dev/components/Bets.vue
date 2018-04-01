291a6c8a6828c2a705699d0cd5d71f857f49e564
Some User <someuser94@mail.ru>
2018-04-01 22:29:34 +0200

    Released switcher logic

--- front-dev/components/Bets.vue
+++ front-dev/components/Bets.vue
@@ -1,15 +1,18 @@
 <template lang="pug">
 div#bet-slip
     div#bet-types
-        span(@click="type='waiting'") Waiting to confirm
-        span(@click="type='confirmed'") Confirmed bets
-        span(@click="type='results'") Results
+        span(
+        v-for="item in menu",
+        @click="changeMenu(item)",
+        :class="item.classes"
+        ) {{item.title}}
     app-bets-waiting(v-if="type=='waiting'")
     app-bets-confirmed(v-if="type=='confirmed'")
     app-bets-results(v-if="type=='results'")
 </template>
 
 <script>
+import _ from "lodash";
 import appBetsWaiting from "./BetsWaiting.vue";
 import appBetsConfirmed from "./BetsConfirmed.vue";
 import appBetsResults from "./BetsResults.vue";
@@ -22,16 +25,65 @@ export default {
     },
     data() {
         return {
-            type: "waiting"
+            type: "waiting",
+            menu: [
+                {
+                    type: "waiting",
+                    classes: "selected",
+                    title: "Waiting to confirm"
+                },
+                {
+                    type: "confirmed",
+                    classes: "",
+                    title: "Confirmed bets"
+                },
+                {
+                    type: "results",
+                    classes: "",
+                    title: "Results"
+                }
+            ]
         }
+    },
+    computed: {
+        classes() {
+            return {
+            
+            }
+        },
+    },
+    methods: {
+        changeMenu(selectedItem) {
+            _.each(this.menu, item => item.classes = item.type === selectedItem.type ? "selected" : "");
+            this.type = selectedItem.type;
+        },
     }
 }
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
+.justify {
+    justify-content: space-around;
+    justify-content: space-evenly;
+}
 #bet-types {
     background: white;
+    display: flex;
+    margin: 10px 0;
+    .justify;
     
+    span {
+        margin: 4px 2px;
+        padding: 5px 10px;
+        background: #33ff33;
+        flex: 1 1 auto;
+        text-align: center;
+        font-size: 17px;
+        cursor: pointer;
     
+        &:first-of-type {margin-left: 4px}
+        &:last-of-type {margin-right: 4px}
+        &.selected {background: #009933; color: white;}
+    }
 }
 </style>
