3772dea73f4938ebcc1476633aa83719762663f7
Sándor Levcsák <levchak.sanek@gmail.com>
2018-11-16 03:37:57 +0100

    Fixed vue config

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,17 +1,7 @@
 module.exports = {
-    pluginOptions: {
-        i18n: {
-            locale: "en",
-            fallbackLocale: "en",
-            localeDir: "locales",
-            enableInSFC: false
-        },
-        moment: {
-            locales: ["en"],
-        }
+    devServer: {
+        proxy: "http://localhost:3000",
     },
 
-    devServer: {
-        proxy: 'http://localhost:3000'
-    },
+    productionSourceMap: false,
 };
